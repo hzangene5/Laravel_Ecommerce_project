d9a88d0a125236b56406b076895c46907311ea65
hassan <hzangene5@gmail.com>
2022-05-18 02:47:14 +0430

    categories complete

---
+++ app/Http/Controllers/Admin/ProductImageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductImageController extends Controller
+{
+    public function upload($primaryImage, $images){
+        
+        $fileNamePrimaryImage = generateFileName( $primaryImage->getClientOriginalName());        
+        $primaryImage->move(public_path(env('PRODUCT_IMAGE_UPLOAD_PATH')), $fileNamePrimaryImage);
+        
+        $fileNameImages = [];
+
+        foreach($images as $image){
+            $fileNameImage = generateFileName( $images->getClientOriginalName());        
+            $image->move(public_path(env('PRODUCT_IMAGE_UPLOAD_PATH')), $fileNameImage);
+
+            array_push($fileNameImages, $fileNameImage);
+        }
+        return ['fileNamePrimaryImage' => $fileNamePrimaryImage, 'fileNameImages' =>$fileNameImages];
+    }
+}
--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -18,7 +18,7 @@ create products
     // get the file name
     var fileName = $(this).val();
     // replace the "choose a file" label
-    $(this).next('custom-file-lable').html(fileName);
+    $(this).next('custom-file-label').html(fileName);
 
   });
 
@@ -27,7 +27,7 @@ create products
     // get the file name
     var fileName = $(this).val();
     // replace the "choose a file" label
-    $(this).next('custom-file-lable').html(fileName);
+    $(this).next('custom-file-label').html(fileName);
 
   });
 
@@ -35,19 +35,23 @@ create products
     'title': 'انتخاب دسته بندی'
   });
 
+  $('#attributesContainer').hide();
+
   $('#categorySelect').on('changed.bs.select', function() {
     let categoryId = $(this).val();
 
     $.get(`{{ url('/admin-panel/management/category-attributes/${categoryId}') }}`, function(response,
-      status ) {
+      status) {
       if (status == 'success') {
-        //console.log(response.attrubtes);
+
+        $('#attributesContainer').fadeIn();
+
 
         // Empty Attribute Container
         $('#attributes').find('div').remove();
 
         // Create and Append Attributes Input
-        response.attrubtes.forEach(attribute => {
+        response.attributes.forEach(attribute => {
           let attributeFormGroup = $('<div/>', {
             class: 'form-group col-md-3'
           });
@@ -66,7 +70,7 @@ create products
           $('#attributes').append(attributeFormGroup);
 
         });
-
+        $('#variationName').text(response.variation.name);
 
       } else {
         alert('مشکل در دریافت لیست خصوصیت ها');
@@ -78,6 +82,8 @@ create products
 
 
   });
+
+  $("#czContainer").czMore();
 </script>
 
 @endsection
@@ -98,7 +104,7 @@ create products
       <div class="form-row">
         <div class="form-group col-md-3">
           <label for="name">نام</label>
-          <input class="form-control" id="name" name="name" type="text">
+          <input class="form-control" id="name" name="name" type="text" value="{{ old('name') }}" >
         </div>
 
         <div class="form-group col-md-3 ">
@@ -142,7 +148,7 @@ create products
         <div class="form-group col-md-3">
           <label for="primary_image"> انتخاب تصویر اصلی</label>
           <div class="custom-file">
-            <input type="file" name="primary_image" class="primary_image-input" id="primary_image">
+            <input type="file" name="primary_image" class="custom-file-input" id="primary_image">
             <label class="custom-file-label" for="primary_image"> انتخاب فایل </label>
 
           </div>
@@ -180,9 +186,60 @@ create products
         </div>
 
         <div id="attributesContainer" class="col-md-12">
-          <div id="attributes" class="row">
+          <div id="attributes" class="row"></div>
+          <div class="col-md-12">
+            <hr>
+            <p>افزودن قیمت و موجودی برای متغییر <span class="font-weight-bold" id="variationName"></span> :</p>
           </div>
+
+          <div id="czContainer">
+            <div id="first">
+              <div class="recordset">
+                <div class="row">
+                  <div class="form-group col-md-3">
+                    <label>نام</label>
+                    <input class="form-control" name="variation_values[value][]" type="text">
+                  </div>
+
+                  <div class="form-group col-md-3">
+                    <label>قیمت</label>
+                    <input class="form-control" name="variation_values[price][]" type="text">
+                  </div>
+
+                  <div class="form-group col-md-3">
+                    <label>تعداد</label>
+                    <input class="form-control" name="variation_values[quantity][]" type="text">
+                  </div>
+
+                  <div class="form-group col-md-3">
+                    <label>شناسه انبار</label>
+                    <input class="form-control" name="variation_values[sku][]" type="text">
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
+
+        {{-- Delivery Section --}}
+
+        <div class="col-md-12">
+          <hr>
+          <p>  هزینه ارسال : </p>
+        </div>
+
+        <div class="form-group col-md-3">
+          <label for="delivery_amount">هزینه ارسال</label>
+          <input class="form-control" id="delivery_amount" name="delivery_amount" type="text" value="{{ old('delivery_amount') }}">
+        </div>
+
+        <div class="form-group col-md-3">
+          <label for="delivery_amount_per_product">هزینه ارسال به ازای محصول اضافی</label>
+          <input class="form-control" id="delivery_amount_per_product" name="delivery_amount_per_product" type="text" value="{{old('delivery_amount_per_product') }}">
+        </div>
+
       </div>
       <button class="btn btn-outline-primary mt-5" type="submit">ثبت</button>
       <a href="{{route('admin.products.index')}}" class="btn btn-dark mt-5 mr-3">بازگشت</a>
